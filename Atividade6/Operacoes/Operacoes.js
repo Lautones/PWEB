19c92f37b245f04976e38127987ce41ce7731673
Matheus Lauton <mlautonsantana@gmail.com>
2025-09-24 21:46:02 -0300

    Add files via upload

---
+++ Atividade6/Operacoes/Operacoes.js
@@ -0,0 +1,9 @@
+let primeiroNumero = parseFloat(prompt("Digite o primeiro número"));
+let segundoNumero = parseFloat(prompt("Digite o segundo número"));
+
+alert(primeiroNumero + segundoNumero);
+alert(primeiroNumero - segundoNumero);
+alert(primeiroNumero * segundoNumero);
+alert(primeiroNumero / segundoNumero);
+alert(primeiroNumero % segundoNumero);
+
