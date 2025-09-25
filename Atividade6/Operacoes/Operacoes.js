02cd792798a21d8f9d9335492c6ee063131258b5
Matheus Lauton <mlautonsantana@gmail.com>
2025-09-24 21:52:35 -0300

    Add files via upload

---
+++ Atividade6/Operacoes/Operacoes.js
@@ -0,0 +1,10 @@
+let primeiroNumero = parseFloat(prompt("Digite o primeiro número"));
+let segundoNumero = parseFloat(prompt("Digite o segundo número"));
+
+alert(`
+    Soma: ${primeiroNumero + segundoNumero}\n
+    Subtração: ${primeiroNumero - segundoNumero}\n
+    Produto: ${primeiroNumero * segundoNumero}\n
+    Divisão: ${primeiroNumero / segundoNumero}\n
+    Resto: ${primeiroNumero % segundoNumero}`);
+
